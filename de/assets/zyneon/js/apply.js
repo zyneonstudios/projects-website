b4c3a07c51e380ad0e4e699912b78b5c949e8b2d
nerotvlive <daniel.niesmann@outlook.com>
2024-06-23 16:14:07 +0200

    Update apply site

---
+++ de/assets/zyneon/js/apply.js
@@ -0,0 +1,5 @@
+urlParams = new URLSearchParams(window.location.search);
+if(urlParams.get("from")) {
+    const from = urlParams.get("from");
+    history.pushState({}, '', window.location.pathname);
+}
